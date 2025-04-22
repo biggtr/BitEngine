b3a61bbc77d88d6367599e16f9612358aabc6a6d
biggtr <bigtorresg@gmail.com>
2025-04-22 14:19:16 +0200

    Added TimeStamps

--- Bit/Src/Bit/Core/Application.cpp
+++ Bit/Src/Bit/Core/Application.cpp
@@ -3,26 +3,43 @@
 
 namespace BitEngine
 {
+
     
-    Application::Application()
+Application::Application()
+{
+}
+
+void Application::Init()
+{
+    //default logic goes before OnInit
+    OnInit();
+}
+void Application::Render()
+{
+    //default logic goes before OnRender
+    OnRender();
+}
+void Application::Update(float deltaTime)
+{
+    //default logic goes before OnUpdate 
+    OnUpdate(deltaTime);
+}
+
+void Application::Run()
+{
+    while(m_IsRunning)
     {
-
-        
-
+        Update(m_Time.GetDeltaTime());
+        Render();
     }
+}
+
+
+
+Application::~Application()
+{
     
-
-    void Application::Run()
-    {
-        while(true);
-    }
-
-
-
-    Application::~Application()
-    {
-        
-    }
+}
 
     
 }
--- Bit/Src/Bit/Core/Application.h
+++ Bit/Src/Bit/Core/Application.h
@@ -1,15 +1,37 @@
 #pragma once
+#include "Bit/Core/TimeStamp.h"
 namespace BitEngine
 {
 
         
-    class Application
-    {
-    public:
-        Application();
-        virtual ~Application();
+class Application
+{
+public:
 
-        virtual void Run();
-   };
-   Application* CreateApplication();
+
+    Application();
+    virtual ~Application();
+    void Run();
+
+private:
+    void Init();
+    void Update(float deltaTime);
+    void Render();
+
+
+protected:
+
+    virtual void OnInit();
+    virtual void OnUpdate(float deltaTime);
+    virtual void OnRender();
+
+
+private:
+    bool m_IsRunning;
+    Time m_Time;
+    
+
+};
+Application* CreateApplication();
+
 }
