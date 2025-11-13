555732d68a745731e55979c9d5e75f91654a519b
biggtr <bigtorresg@gmail.com>
2025-11-13 22:12:17 +0200

    Added character movement

--- Games/Dystopia/src/Dystopia.cpp
+++ Games/Dystopia/src/Dystopia.cpp
@@ -7,6 +7,7 @@
 #include "Bit/ECS/Entity.h"
 #include "Bit/ECS/Systems/InputSystem.h"
 #include "Bit/ECS/Systems/System.h"
+#include <exception>
 
 void Dystopia::Initialize()
 {
@@ -14,12 +15,12 @@ void Dystopia::Initialize()
     player.AddComponent<BitEngine::TransformComponent>();
 
     auto& playerTransform = player.GetComponent<BitEngine::TransformComponent>();
-    playerTransform.Position = BMath::Vec3(0.0f, 0.0f, -5.0f);
+    playerTransform.Position = BMath::Vec3(100.0f, 300.0f, -5.0f);
     playerTransform.Rotation = BMath::Vec3(0.0f, 0.0f, 0.0f);
     playerTransform.Scale = {35.0f, 35.0f, 1.0f};
+
     player.AddComponent<BitEngine::SpriteComponent>();
     auto& playerSprite = player.GetComponent<BitEngine::SpriteComponent>();
-    
     m_AssetManager->AddTexture("samuri", "assets/textures/samuri.png");
     playerSprite.STexture = m_AssetManager->GetTexture("samuri");
     playerSprite.Width = 21 * 32;
@@ -34,6 +35,11 @@ void Dystopia::Initialize()
     m_Animation2DSystem->CreateAnimation(player, "PlayerAttack", 7, 22, 0.07f);
     m_Animation2DSystem->CreateAnimation(player, "PlayerDamaged", 4, 29, 0.07f);
     m_Animation2DSystem->CreateAnimation(player, "PlayerIdle", 10, 29 + 4, 0.07f);
+
+    player.AddComponent<Character2DControllerComponent>();
+    auto& collision = player.AddComponent<CollisionComponent>();
+    collision.Width = 28.0f;
+    collision.Height = 32.0f;
 
     m_AssetManager->AddTexture("dystopiaTileset", "assets/textures/dystopiatiles.png");
 
@@ -57,6 +63,26 @@ void Dystopia::Render3D()
 }
 void Dystopia::Update(f32 deltaTime)
 {
+    auto& transform = player.GetComponent<BitEngine::TransformComponent>();
+    auto& controller = player.GetComponent<Character2DControllerComponent>();
+    auto& collision = player.GetComponent<CollisionComponent>();
+
+    HandleInput(controller, deltaTime);
+
+    HandleMovement(controller, deltaTime);
+
+    HandleJump(controller, deltaTime);
+
+    HandleGravity(controller, deltaTime);
+
+    ApplyPhysics(transform, controller, deltaTime);
+
+    HandleCollision(transform, controller, collision);
+
+    HandleGroundDetection(transform, controller, collision);
+
+    UpdateAnimation(controller, transform);
+            
     auto& playertransform = player.GetComponent<BitEngine::TransformComponent>();
     if(BitEngine::InputIsKeyDown(BitEngine::KEY_D))
     {
@@ -71,3 +97,129 @@ void Dystopia::Update(f32 deltaTime)
 
 }
 
+void Dystopia::HandleInput(Character2DControllerComponent& controller, f32 deltaTime)
+{
+    b8 wasJumpPressed = controller.JumpPressed;
+
+    if(BitEngine::InputIsKeyDown(BitEngine::KEY_D))
+    {
+        controller.MoveInput = 1.0f;
+    }
+    if(BitEngine::InputIsKeyDown(BitEngine::KEY_A))
+    {
+        controller.MoveInput = -1.0f;
+    }
+
+    controller.JumpHeld = BitEngine::InputIsKeyDown(BitEngine::KEY_SPACE);
+    controller.JumpPressed = controller.JumpHeld && !wasJumpPressed;
+    controller.JumpReleased = !controller.JumpHeld && wasJumpPressed;
+
+    if(controller.JumpPressed)
+        controller.JumpBufferTimer = controller.JumpBufferTime;
+
+    else if(controller.JumpBufferTimer > 0.0f)
+        controller.JumpBufferTimer -= deltaTime;
+}
+
+void Dystopia::HandleMovement(Character2DControllerComponent& controller, f32 deltaTime)
+{
+    f32 targetSpeed = controller.MoveInput * controller.MaxSpeed;
+    f32 currentSpeed = controller.Velocity.x;
+    f32 accelerationRate = controller.Acceleration;
+
+    if(!controller.IsGrounded)
+    {
+        accelerationRate *= controller.AirControl;
+    }
+
+    if(targetSpeed > 0.0f && currentSpeed < 0.0f || targetSpeed < 0.0f && currentSpeed > 0.0f) // if target speed was going right and i was moving left and turned the other dir accelerate faster to make it smooth
+        accelerationRate *= 1.5f;
+                                                                                               
+    if(targetSpeed == 0.0f && controller.IsGrounded)
+        accelerationRate = controller.Deceleration;
+
+    f32 speedDiff = targetSpeed - currentSpeed;
+    f32 movement = speedDiff * accelerationRate * deltaTime;
+
+    //clamp movement
+    if (fabs(movement) > fabs(speedDiff))
+        movement = speedDiff;
+
+    controller.Velocity += movement;
+}
+
+void Dystopia::HandleJump(Character2DControllerComponent& controller, f32 deltaTime)
+{
+    if(controller.IsGrounded)
+        controller.CoyoteTimer = controller.CoyoteTime;
+
+    else if(controller.CoyoteTimer > 0.0f)
+        controller.CoyoteTimer -= deltaTime;
+
+    b8 canJump = (controller.CoyoteTimer > 0.0f && controller.JumpCount == 0) || (controller.JumpCount < controller.MaxJumps);
+
+    if(controller.JumpBufferTimer > 0.0f && canJump)
+    {
+        controller.Velocity.y = controller.JumpForce;
+        controller.IsJumping = true;
+        controller.JumpCount++;
+
+        controller.JumpBufferTimer = 0.0f;
+        controller.CoyoteTimer = 0.0f;
+    }
+
+    // if the jump button was released mid air
+    if (controller.JumpReleased && controller.Velocity.y > 0.0f)
+        controller.Velocity.y *= 0.5f;
+    
+    
+}
+
+void Dystopia::HandleGravity(Character2DControllerComponent& controller, f32 deltaTime)
+{
+    if (controller.IsGrounded && controller.Velocity.y <= 0.0f)
+    {
+        controller.Velocity.y = 0.0f;
+        controller.IsJumping = false;
+        return;
+    }
+    f32 gravity = (controller.Velocity.y > 0.0f) ? controller.RisingGravity : controller.FallingGravity;
+
+    controller.Velocity.y -= gravity * deltaTime;
+
+    if (controller.Velocity.y < -controller.TerminalVelocity)
+        controller.Velocity.y = -controller.TerminalVelocity;
+}
+
+void Dystopia::ApplyPhysics(BitEngine::TransformComponent& transform, Character2DControllerComponent& controller, f32 deltaTime)
+{
+    transform.Position += controller.Velocity * deltaTime;
+}
+
+void Dystopia::HandleCollision(BitEngine::TransformComponent& transform, Character2DControllerComponent& controller, CollisionComponent& collision)
+{
+
+}
+
+void Dystopia::HandleGroundDetection(BitEngine::TransformComponent& transform, Character2DControllerComponent& controller, CollisionComponent& collision);
+
+void Dystopia::UpdateAnimation(Character2DControllerComponent& controller, BitEngine::TransformComponent& transform)
+{
+    if (controller.MoveInput > 0.0f)
+        transform.Scale.x = abs(transform.Scale.x);
+    else if (controller.MoveInput < 0.0f)
+        transform.Scale.x = -abs(transform.Scale.x); 
+    
+    if (!controller.IsGrounded)
+    {
+        m_Animation2DSystem->SetAnimation(player, "PlayerIdle");  // jump animation 
+    }
+    else if (abs(controller.Velocity.x) > 10.0f)
+    {
+        m_Animation2DSystem->SetAnimation(player, "PlayerRun");
+    }
+    else
+    {
+        m_Animation2DSystem->SetAnimation(player, "PlayerIdle");
+    }
+}
--- Games/Dystopia/src/Dystopia.h
+++ Games/Dystopia/src/Dystopia.h
@@ -1,12 +1,52 @@
 #pragma once 
 #include "Bit/Core/Application.h"
+#include "Bit/ECS/Compontents.h"
 #include "Bit/ECS/Entity.h"
 #include <Bit/Core/Game.h>
 
+struct Character2DControllerComponent
+{
+    BMath::Vec3 Velocity;
+
+    f32 MaxSpeed = 180.0f;
+    f32 Acceleration = 1500.0f;
+    f32 Deceleration = 2000.0f;
+    f32 AirControl = 0.85f;
+    
+    f32 JumpForce = 380.0f;
+    f32 RisingGravity = 1000.0f;
+    f32 FallingGravity = 1800.0f;
+    f32 TerminalVelocity = 500.0f;
+
+    b8 IsGrounded = false;
+    b8 IsJumping = false;
+    i32 JumpCount = 0;
+    i32 MaxJumps = 2;  
+
+    float CoyoteTime = 0.12f; // gives the player some time after leaving the ground to jump (beep beep)
+    float CoyoteTimer = 0.0f; // counter till the coyotetime to see how much time left for coyote jumping
+    float JumpBufferTime = 0.12f; // allows the player to jump before touching the ground again from falling makes it not laggy and responsive
+    float JumpBufferTimer = 0.0f;
+
+    float MoveInput = 0.0f;  // -1, 0, 1
+    bool JumpPressed = false;
+    bool JumpHeld = false;
+    bool JumpReleased = false;
+};
+struct CollisionComponent
+{
+    f32 Width = 32.0f;
+    f32 Height = 32.0f;
+    BMath::Vec3 Offset;
+
+    b8 CollidingBelow = false;
+    b8 CollidingAbove = false;
+    b8 CollidingLeft = false;
+    b8 CollidingRight = false;
+};
 class Dystopia : public BitEngine::Game
 {
 public:
-    BitEngine::Entity player;
     Dystopia()
     {
     }
@@ -19,5 +59,23 @@ public:
     virtual void RenderUI() override; 
 
 private:
-    u32 currentTexture;
+
+    void HandleInput(Character2DControllerComponent& controller, f32 deltaTime);
+
+    void HandleMovement(Character2DControllerComponent& controller, f32 deltaTime);
+
+    void HandleJump(Character2DControllerComponent& controller, f32 deltaTime);
+
+    void HandleGravity(Character2DControllerComponent& controller, f32 deltaTime);
+
+    void ApplyPhysics(BitEngine::TransformComponent& transform, Character2DControllerComponent& controller, f32 deltaTime);
+
+    void HandleCollision(BitEngine::TransformComponent& transform, Character2DControllerComponent& controller, CollisionComponent& collision);
+
+    void HandleGroundDetection(BitEngine::TransformComponent& transform, Character2DControllerComponent& controller, CollisionComponent& collision);
+
+    void UpdateAnimation(Character2DControllerComponent& controller, BitEngine::TransformComponent& transform);
+private:
+    BitEngine::Entity player;
+
 };
